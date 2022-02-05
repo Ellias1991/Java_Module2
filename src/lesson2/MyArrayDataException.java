0b5be242f119c4d6ab98022f6a9e6df3bf816754
geekBrains <info@geekBrains.ru>
2022-02-06 00:38:16 +0300

    код исправлен и доработан окончательно

--- src/lesson2/MyArrayDataException.java
+++ src/lesson2/MyArrayDataException.java
@@ -1,12 +1,26 @@
 package lesson2;
 
-public class MyArrayDataException extends ArrayStoreException {
+public class MyArrayDataException extends RuntimeException {
+private int rowI;
+private int colJ;
 
+    public int getRowI() {
+        return rowI;
+    }
+
+    public int getColJ() {
+        return colJ;
+    }
+public MyArrayDataException(String message,int rowI, int colJ){
+        super (message);
+        this.rowI=rowI;
+        this.colJ=colJ;
+}
     public static void main(String[] args) {
 
         try {
             String[][] arr = new String[][]{
-                    {"1", "2q", "3", "2"},
+                    {"1", "2", "3", "2"},
                     {"1", "5", "7", "64"},
                     {"1", "2", "38", "2"},
                     {"9", "5", "4", "78"},
@@ -20,7 +34,7 @@ public class MyArrayDataException extends ArrayStoreException {
                     sum += arr1[i][j];
                     System.out.println(sum);
                 }
-                throw new MyArrayDataException();
+
             }
         } catch (MyArrayDataException e) {
             e.printStackTrace();
