652ca6bc49bfa00e3ae542480733e2cf12f4a0ae
geekBrains <info@geekBrains.ru>
2022-02-08 01:10:46 +0300

    второй коммит

---
+++ src/lesson3/Count.java
@@ -0,0 +1,33 @@
+package lesson3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Count {
+    public static void main(String[] args) {
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Pear");
+        fruits.add("Banana");
+        fruits.add("Orange");
+        fruits.add("Tomato");
+        fruits.add("Lemon");
+        fruits.add("Melon");
+        fruits.add("Tomato");
+        fruits.add("Orange");
+        fruits.add("Apple");
+        fruits.add("Pear");
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Melon");
+        fruits.add("Peach");
+        fruits.add("Orange");
+        fruits.add("Banana");
+        fruits.add("Lemon");
+int sum=0;
+for(String name:fruits){
+    System.out.println(fruits );
+}
+
+    }
+}
