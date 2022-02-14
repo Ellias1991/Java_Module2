ee3ed8e72c0d0decac9279a565b146e84e66aa52
geekBrains <info@geekBrains.ru>
2022-02-14 23:56:31 +0300

    Третий коммит,задание выполнено полностью,доработаны оба метода.

--- src/lesson5/MyHw5Thread.java
+++ src/lesson5/MyHw5Thread.java
@@ -8,6 +8,7 @@ public class MyHw5Thread {
 
     public static void main(String[] args) {
         MethodOne();
+        MethodTwo();
     }
 
     public static void MethodOne() {
@@ -19,7 +20,7 @@ public class MyHw5Thread {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
         System.out.println("MethodOne execution time: " + (System.currentTimeMillis() - startTime) + " ms.");
-        System.out.println(arr[HALF]);
+
     }
 
     public static void MethodTwo() {
@@ -27,11 +28,34 @@ public class MyHw5Thread {
         System.out.println(Arrays.toString(MajorArray));
 
         long startTime = System.currentTimeMillis();
-        float[] leftSide = new float[3];
-        float[] RightSide = new float[3];
-        System.arraycopy(MajorArray, 0, leftSide, 0, 3);
-        System.arraycopy(MajorArray, 3, RightSide, 0, 3);
+        float[] LeftSize = new float[3];
+        float[] RightSize = new float[3];
+        System.arraycopy(MajorArray, 0, LeftSize, 0, 3);
+        System.arraycopy(MajorArray, 3, RightSize, 0, 3);
 
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < LeftSize.length; i++) {
+                LeftSize[i] = (float) (LeftSize[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < RightSize.length; i++) {
+                RightSize[i] = (float) (RightSize[i] * Math.sin(0.2f + (i + 3) / 5) * Math.cos(0.2f + (i + 3) / 5) * Math.cos(0.4f + (i + 3) / 2));
+            }
+        });
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+            System.arraycopy(LeftSize, 0, MajorArray, 0, 3);
+            System.arraycopy(RightSize, 0, MajorArray, 0, 3);
+            long endTime = System.currentTimeMillis();
+            System.out.println("MethodTwo execution time:" + (System.currentTimeMillis() - startTime) + " ms.");
+        }
     }
 }
 
