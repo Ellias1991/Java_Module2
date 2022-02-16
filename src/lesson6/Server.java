27b62fe015c6bc376d41321b65ef2110541ad7e4
geekBrains <info@geekBrains.ru>
2022-02-16 23:27:13 +0300

    второй коммит

--- src/lesson6/Server.java
+++ src/lesson6/Server.java
@@ -1,6 +1,7 @@
 package lesson6;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -18,13 +19,17 @@ public class Server {
             socket=server.accept();
             System.out.println("Client connected");
 
-            Scanner sc= new Scanner(socket.getInputStream());//то при помощи чего слушать сообщения,передается инф.в сокет
+            Scanner sc= new Scanner(socket.getInputStream());//слушаем входящий канал,те сообщения которые нам напишет клиент,
+            // то при помощи чего слушать сообщения,передается инф.в сокет
+            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);//надо записывать исходящий канал.использует буфер,
+            // копит сообщения,как накопит-он их отправляет.В данном случае сразу просим Автофшалем не копить,а отправлять сообщения.
             while (true){
                 String str= sc.nextLine();
                 if (str.equals("/end")){
                     break;
                 }
                 System.out.println("Client:"+ str);
+                out.println("ECHO:"+ str);
             }
 
         } catch (IOException e) {
