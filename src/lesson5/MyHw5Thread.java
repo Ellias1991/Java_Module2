4ecdf9d291b289ede2572e50116740ae6cf51d57
geekBrains <info@geekBrains.ru>
2022-02-20 20:56:30 +0300

    третий коммит,чат работает

---
+++ src/lesson5/MyHw5Thread.java
@@ -0,0 +1,66 @@
+package lesson5;
+
+import java.util.Arrays;
+
+public class MyHw5Thread {
+
+    private static int HALF;
+
+    public static void main(String[] args) {
+        MethodOne();
+        MethodTwo();
+    }
+
+    public static void MethodOne() {
+        final int SIZE = 10_000_000;
+        final int HALF = SIZE / 2;
+        float[] arr = new float[SIZE];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1.0f;
+        }
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println("MethodOne execution time: " + (System.currentTimeMillis() - startTime) + " ms.");
+
+    }
+
+    public static void MethodTwo() {
+        float[] MajorArray = {5.0f, 4.0f, 3.0f, 2.0f, 1.0f, 8.0f};
+        System.out.println(Arrays.toString(MajorArray));
+
+        long startTime = System.currentTimeMillis();
+        float[] LeftSize = new float[3];
+        float[] RightSize = new float[3];
+        System.arraycopy(MajorArray, 0, LeftSize, 0, 3);
+        System.arraycopy(MajorArray, 3, RightSize, 0, 3);
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < LeftSize.length; i++) {
+                LeftSize[i] = (float) (LeftSize[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < RightSize.length; i++) {
+                RightSize[i] = (float) (RightSize[i] * Math.sin(0.2f + (i + HALF) / 5) * Math.cos(0.2f + (i + HALF) / 5) * Math.cos(0.4f + (i + HALF) / 2));
+            }
+        });
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+            System.arraycopy(LeftSize, 0, MajorArray, 0, 3);
+            System.arraycopy(RightSize, 0, MajorArray, 0, 3);
+            long endTime = System.currentTimeMillis();
+            System.out.println("MethodTwo execution time:" + (System.currentTimeMillis() - startTime) + " ms.");
+        }
+    }
+}
+
+
---
+++ src/lesson6/ClientHW.java
@@ -0,0 +1,61 @@
+package lesson6;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ClientHW {
+
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 8189;
+
+    public static void main(String[] args) throws IOException {
+
+        Socket socket = null;
+        Scanner scanner = new Scanner(System.in);
+        try {
+            socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            System.out.println("Подключен к серверу: " + socket.getRemoteSocketAddress());
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+
+
+            Thread threadReader = new Thread(() -> {
+                try {
+                    while (true) {
+                        outputStream.writeUTF(scanner.nextLine());
+                    }
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            threadReader.setDaemon(true);
+            threadReader.start();
+
+            while (true) {
+                String str = inputStream.readUTF();
+                if (str.equals("/close")) {
+                    System.out.println("Потеряно соединение с сервером");
+                    outputStream.writeUTF("/close");
+                    break;
+                } else {
+                    System.out.println("Сервер: " + str);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException | NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
